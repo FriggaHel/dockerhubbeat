cdeb519ab9902d0791a940725970ae0485cb0841
Felix P <felix@wingz.me>
2017-06-20 01:03:38 +0200

    Raise error when unable to read repository stats (+Warning)

--- beater/dockerhubbeat.go
+++ beater/dockerhubbeat.go
@@ -2,15 +2,15 @@ package beater
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/FriggaHel/dockerhubbeat/config"
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/libbeat/publisher"
 	"net/http"
 	"time"
-
-	"github.com/FriggaHel/dockerhubbeat/config"
 )
 
 type Dockerhubbeat struct {
@@ -18,31 +18,6 @@ type Dockerhubbeat struct {
 	config config.Config
 	client publisher.Client
 }
-
-/*
-  "user": "library",
-  "name": "traefik",
-  "namespace": "library",
-  "repository_type": "image",
-  "status": 1,
-  "description": "Tr\u00e6f\u026ak, a modern reverse proxy",
-  "is_private": false,
-  "is_automated": false,
-  "can_edit": false,
-  "star_count": 174,
-  "pull_count": 9176629,
-  "last_updated": "2017-06-16T17:02:31.412870Z",
-  "build_on_cloud": null,
-  "has_starred": false,
-  "full_description": "descr",
-  "affiliation": null,
-  "permissions": {
-    "read": true,
-    "write": false,
-    "admin": false
-  }
-}
-*/
 
 type DockerhubPerms struct {
 	Read  bool `json:"read"`
@@ -95,7 +70,11 @@ func (bt *Dockerhubbeat) Run(b *beat.Beat) error {
 		}
 
 		logp.Info(bt.config.Repository)
-		data, _ := bt.FetchData()
+		data, err := bt.FetchData()
+		if err != nil {
+			logp.Warn(fmt.Sprintf("Unable to fetch repository data [%s]", bt.config.Repository))
+			continue
+		}
 
 		event := common.MapStr{
 			"@timestamp":       common.Time(time.Now()),
@@ -116,16 +95,19 @@ func (bt *Dockerhubbeat) Stop() {
 
 func (bt *Dockerhubbeat) FetchData() (DockerhubData, error) {
 	res := DockerhubData{}
-	r, err := http.Get("https://hub.docker.com/v2/repositories/library/traefik/")
-	if err != nil {
+	r, err := http.Get(fmt.Sprintf("https://hub.docker.com/v2/repositories/%s", bt.config.Repository))
+	if err != nil || r.StatusCode != 200 {
 		return res, err
 	}
+	defer r.Body.Close()
+
 	err = json.NewDecoder(r.Body).Decode(&res)
 	if err != nil {
 		return res, err
 	}
-	r.Body.Close()
 
-	fmt.Println(res)
+	if res.Name == "" {
+		return res, errors.New("Unable to unmarshal repository data")
+	}
 	return res, nil
 }
